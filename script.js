69176dc9e07741e5089d405d06c41fb6df9cd277
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-09-10 22:25:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ let thanks = document.querySelector('div#card-thanks')
 
 thanks.style.display = "none"
 
-
-
-
-
 function submit() {
-
-    
 
     let card = document.querySelector('div.card')
     card.style.display = "none"
