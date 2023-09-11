cd48a5dad12f9563bb159a8a3387575857ad59e3
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-09-10 22:56:36 -0300

    javascript feito

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 let thanks = document.querySelector('div#card-thanks')
-
 thanks.style.display = "none"
 
-function submit() {
+let card = document.querySelector('div#card')
 
-    let card = document.querySelector('div.card')
-    card.style.display = "none"
+document.addEventListener("DOMContentLoaded", function () {
+    const lista = document.getElementById("lista");
+    const resultado = document.querySelector("div.res-txt");
+    const submitBtn = document.getElementById("submitBtn");
 
-    thanks.style.display = "inline-flex"
+    let itemSelecionado = null;
 
-    var res = document.querySelector('div.res-txt')
-}
+    // Adicionar um ouvinte de evento de clique para cada <li>
+    const itens = lista.querySelectorAll("li");
+    itens.forEach(function (item) {
+        item.addEventListener("click", function () {
+            // Remove a classe de seleção de todos os itens
+            itens.forEach(function (i) {
+                i.classList.remove("selecionado");
+            });
+
+            // Adiciona uma classe de seleção ao item clicado
+            item.classList.add("selecionado");
+            itemSelecionado = item.textContent;
+        });
+    });
+
+    // Ouvinte de evento para o botão "Submit"
+    submitBtn.addEventListener("click", function () {
+        if (itemSelecionado) {
+            resultado.textContent = `You selected ${itemSelecionado} out of 5`;
+
+            thanks.style.display = "inline-flex"
+
+            card.style.display = "none"
+        } else {
+            alert('Please select a number.')
+        }
+    });
+});
