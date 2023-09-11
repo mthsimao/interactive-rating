0c6ca2ec21e879e695d7298c08f9831c86d4494e
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-09-10 22:24:58 -0300

    html desktop feito

---
+++ script.js
@@ -0,0 +1,19 @@
+let thanks = document.querySelector('div#card-thanks')
+
+thanks.style.display = "none"
+
+
+
+
+
+function submit() {
+
+    
+
+    let card = document.querySelector('div.card')
+    card.style.display = "none"
+
+    thanks.style.display = "inline-flex"
+
+    var res = document.querySelector('div.res-txt')
+}
